956859138e8c4802cd572efdeef9893a1a46f880
Dario Oddenino <branch13@gmail.com>
2021-04-30 15:49:56 +0200

    more wip

--- src/Haskchimp.hs
+++ src/Haskchimp.hs
@@ -15,12 +15,14 @@ where
 
 import           Control.Monad.IO.Class
 import           Data.Aeson
+import           Data.ByteString.Char8      (pack)
 import qualified Data.ByteString.Lazy.Char8 as L8
-import           Data.ByteString.Char8  (pack)
+import           Data.Digest.Pure.MD5
+import           Data.Text                  (unpack)
+import           Debug.Trace
 import           Haskchimp.Types
 import           Network.HTTP.Simple
-import           Text.Email.Parser      (EmailAddress)
-import Debug.Trace
+import           Text.Email.Parser          (EmailAddress)
 
 -- dc is us10 for us
 rootUrl :: String -> String
@@ -36,17 +38,22 @@ journeyTriggerUrl :: String -> JourneyId -> StepId -> String
 journeyTriggerUrl dc (JourneyId j) (StepId s) =
   rootUrl dc <> "/customer-journeys/journeys/" <> show j <> "/steps/" <> show s <> "/actions/trigger"
 
+addEventUrl :: String -> ListId -> EmailAddress -> String
+addEventUrl dc (ListId l) emailAddress =
+  let hashedMail = show $ md5 $ L8.pack $ show emailAddress
+  in rootUrl dc <> "/lists/" <> unpack l <> "/members/" <> hashedMail <> "/events"
+
 -- | Temporary test key to try stuff out
 mailchimpkey :: String
 mailchimpkey = "fake-test-key"
 
-handleResponse :: FromJSON a => Response L8.ByteString -> Either String (MResponse a)
+handleResponse :: FromJSON a => Response L8.ByteString -> MResult a
 handleResponse r = if getResponseStatusCode r >= 400
-                   then MFail <$> (eitherDecode $ getResponseBody r)
-                   else MSuccess <$> (eitherDecode $ getResponseBody r)
+                   then MFail <$> eitherDecode (getResponseBody r)
+                   else MSuccess <$> eitherDecode (getResponseBody r)
 
 -- | Helper method for get requests
-get_ :: MonadIO m => FromJSON a => String -> m (Either String (MResponse a))
+get_ :: MonadIO m => FromJSON a => String -> m (MResult a)
 get_ url = liftIO $ do
   request' <- parseRequest $ "GET " <> url
   let request = setRequestHeader "Authorization"
@@ -55,7 +62,7 @@ get_ url = liftIO $ do
   res <- httpLBS request
   pure $ handleResponse res
 
-post_ :: MonadIO m => Show b => FromJSON b => ToJSON a =>  String -> a -> m (Either String (MResponse b))
+post_ :: MonadIO m => FromJSON b => ToJSON a =>  String -> a -> m (MResult b)
 post_ url body = liftIO $ do
   request' <- parseRequest $ "POST " <> url
   let request = setRequestHeader "Authorization"
@@ -65,11 +72,22 @@ post_ url body = liftIO $ do
   res <- httpLBS request
   pure $ handleResponse res
 
+-- EXPORTED METHODS. TODO I'm hardcoding the "us10" datacenter for now
+
 -- | Retrieves the audiences created for this account.
-getLists :: MonadIO m => m (Either String (MResponse Lists))
+getLists :: MonadIO m => m (MResult Lists)
 getLists = get_ (listsUrl "us10")
 
+-- TODO add user to list
 
-journeyTrigger :: MonadIO m => JourneyId -> StepId -> EmailAddress -> m (Either String (MResponse ()))
-journeyTrigger journeyId stepId email = do
+-- | Triggers a journey endpoint sending the email
+journeyTrigger :: MonadIO m => JourneyId -> StepId -> EmailAddress -> m (MResult ())
+journeyTrigger journeyId stepId email =
   post_ (journeyTriggerUrl "us10" journeyId stepId) $ JourneyTriggerPayload email
+
+-- | Sends an event to mailchimp
+addEvent :: MonadIO m => ToJSON a => ListId -> EmailAddress -> EventName -> (Maybe a) -> m (MResult ())
+addEvent listId emailAddress eventName maybeBody = do
+  let url = addEventUrl "us10" listId emailAddress
+      pl = EventPayload eventName maybeBody
+  post_ url pl
--- src/Haskchimp/Types.hs
+++ src/Haskchimp/Types.hs
@@ -5,49 +5,220 @@ Maintainer: Dario Oddenino <branch13@gmail.com>
 
 See README for more info
 -}
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans       #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE DuplicateRecordFields      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE OverloadedStrings          #-}
 module Haskchimp.Types where
 
 import           Data.Aeson
-import           Data.Text
-import           GHC.Generics      (Generic)
-import           Text.Email.Parser (EmailAddress)
+import           Data.Aeson.Types    (parseFail)
+import           Data.Text           hiding (map)
+import           GHC.Generics        (Generic)
+import           Text.Email.Parser   (EmailAddress)
+import           Text.Email.Validate (emailAddress)
+import qualified Data.ByteString.Char8 as B8
 
+-- | A couple of orphan instances to simplify json parsing/encoding
+instance ToJSON EmailAddress where
+  toJSON email = toJSON $ show email
+instance FromJSON EmailAddress where
+  parseJSON = withText "EmailAddress" $ \t ->
+    case emailAddress $ B8.pack $ unpack t of
+      Nothing -> parseFail $ "Invalid email address: " <> unpack t
+      Just e  -> pure e
+
+-- | A Mailchimp Journey ID
 newtype JourneyId = JourneyId Int
+-- | A Journey's Step ID
 newtype StepId = StepId Int
+-- | A List (Audience) ID
+newtype ListId = ListId Text
+  deriving stock (Show, Generic)
+  deriving newtype Eq
+  deriving anyclass (FromJSON, ToJSON)
 
+-- | A name for an Event to be sent to Mailchimp
+newtype EventName = EventName Text
+  deriving stock (Show, Generic)
+  deriving newtype Eq
+  deriving anyclass (FromJSON, ToJSON)
 
 -- Mailchimp types
 
+-- | Mailchimp's json error model
 data MError =
   MError { status :: Int
-         , title :: String
+         , title  :: String
          , detail :: String
          }
-  deriving (Eq, Show, Generic, FromJSON)
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (FromJSON)
 
+-- | Mailchimp returning a failure or a success, both in json format.
 data MResponse a = MFail MError | MSuccess a
-  deriving (Eq, Show)
+  deriving stock (Show, Eq)
 
--- | Get information about all lists (audiences) in the account.
+instance Functor MResponse where
+  fmap f (MSuccess a) = MSuccess $ f a
+  fmap _ (MFail e)    = MFail e
+
+instance Applicative MResponse where
+  pure = MSuccess
+  (MSuccess ab) <*> (MSuccess a) = MSuccess (ab a)
+  MFail e <*> _ = MFail e
+  MSuccess ab <*> b = fmap ab b
+
+-- | We might fail parsing the json.
+-- | TODO maybe we can conflate all the errors
+type MResult a = Either String (MResponse a)
+
+-- LISTS
+
+-- | A list (audience) info
 data List =
-  List { id     :: Text
+  List { id     :: ListId
        , web_id :: Int
        , name   :: Text
        }
-  deriving (Eq, Show, Generic, FromJSON)
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (FromJSON)
 
+-- | All the lists (audiences) of the account
 data Lists =
   Lists { lists       :: [List]
         , total_items :: Int
         }
-  deriving (Eq, Show, Generic, FromJSON)
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (FromJSON)
 
+-- | The email type the customer has chosen
+data EmailType = EmailTypeHTML | EmailTypeText
+  deriving stock (Eq, Show, Generic)
+
+instance ToJSON EmailType where
+  toJSON EmailTypeHTML = toJSON ("html" :: String)
+  toJSON EmailTypeText = toJSON ("text" :: String)
+
+instance FromJSON EmailType where
+  parseJSON = withText "EmailType"
+    $ \case
+        "html" -> pure EmailTypeHTML
+        "text" -> pure EmailTypeText
+        t -> parseFail $ "Invalid email type: " <> unpack t
+
+-- | The list status of a specific address
+data ListMemberStatus
+  = ListMemberStatusSubscribed
+  | ListMemberStatusUnsubscribed
+  | ListMemberStatusCleaned
+  | ListMemberStatusPending
+  deriving stock (Eq, Show, Generic)
+
+instance ToJSON ListMemberStatus where
+  toJSON ListMemberStatusSubscribed   = toJSON ("subscribed" :: String)
+  toJSON ListMemberStatusUnsubscribed = toJSON ("unsubscribed" :: String)
+  toJSON ListMemberStatusCleaned      = toJSON ("cleaned" :: String)
+  toJSON ListMemberStatusPending      = toJSON ("pending" :: String)
+
+instance FromJSON ListMemberStatus where
+  parseJSON = withText "ListMemberStatus"
+    $ \case
+      "subscribed" -> pure ListMemberStatusSubscribed
+      "unsubscribed" -> pure ListMemberStatusUnsubscribed
+      "cleaned" -> pure ListMemberStatusCleaned
+      "pending" -> pure ListMemberStatusPending
+      t -> parseFail $ "Invalid status: " <> unpack t
+
+-- | A list member info used in batch lists update requests.
+data MemberPayload =
+  MemberPayload { email_address :: EmailAddress
+                , status        :: ListMemberStatus
+                , email_type    :: EmailType
+                -- , merge_fields  :: Value --TODO maybe this can be a map?
+                , vip           :: Bool
+                }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass ToJSON
+
+-- | The data sent in batch update requests.
+data ListBatchUpdatePayload =
+  ListBatchUpdatePayload { members         :: [MemberPayload]
+                         , update_existing :: Bool
+                         -- , skip_merge_validation :: Bool -- TODO this are query params..
+                         -- , skip_duplicate_check :: Bool
+                         }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass ToJSON
+
+data ListMemberResult =
+  ListMemberResult { id              :: Text
+                   , email_address   :: EmailAddress
+                   , unique_email_id :: Text
+                   , email_type      :: EmailType
+                   , status          :: ListMemberStatus
+                   -- , merge_fields :: Value -- TODO maybe this can be a map?
+                   , vip             :: Bool
+                   , list_id         :: ListId
+                   -- more fields we will not need
+                   }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToJSON, FromJSON)
+
+-- | The possible error types coming back from a batch list update.
+data ListMemberErrorType = ErrorContactExists | ErrorGeneric
+  deriving stock (Eq, Show)
+instance FromJSON ListMemberErrorType where
+  parseJSON = withText "ListMemberErrorType"
+    $ \case
+      "ERROR_CONTACT_EXISTS" -> pure ErrorContactExists
+      "ERROR_GENERIC" -> pure ErrorGeneric
+      t -> parseFail $ "Invalid ListMemberErrorType: " <> unpack t
+
+-- | The possible error coming back from a list batch update.
+data ListMemberError =
+  ListMemberError { email_address :: EmailAddress
+                  , error :: Text
+                  , error_code :: ListMemberErrorType
+                  }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass FromJSON
+
+-- | The list batch update response
+data ListBatchUpdateResponse =
+  ListBatchUpdateResponse { new_members     :: [ListMemberResult]
+                          , updated_members :: [ListMemberResult]
+                          , errors          :: [ListMemberError]
+                          , total_created   :: Int
+                          , total_updated   :: Int
+                          , error_count     :: Int
+                          }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass FromJSON
+
+-- JOURNEYS
+
+-- | The data sent in a journey trigger
 data JourneyTriggerPayload =
   JourneyTriggerPayload { email_address :: EmailAddress }
-  deriving (Eq, Show, Generic)
+  deriving stock (Eq, Show, Generic)
 
+-- | TODO doing a test with foo to see if it's used. We can derive it normally afterwards
 instance ToJSON JourneyTriggerPayload where
   toJSON (JourneyTriggerPayload e) = object [ "email_address" .= show e, "foo" .= ("test test" :: String) ]
+
+ -- EVENTS
+
+-- | The data sent in an event trigger.
+data EventPayload a =
+  EventPayload { name       :: EventName
+               , properties :: Maybe a
+               -- , is_syncing :: Bool
+               -- , occurred_at :: ISO 8601 date
+               }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToJSON)
